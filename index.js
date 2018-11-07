173efadef481e1d960fc1f638b03c19b1ee0ced1
Akın Özgen <akinozgen17@outlook.com>
2018-11-07 06:32:18 +0300

    test

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const ytdl = require('ytdl-core');
 const app = express();
 
 app.get('/:videoId', function (req, res) {
 
-  res.send(req.params);
+  const videoUrl = 'https://www.youtube.com/watch?v=' + req.params.videoId;
+  const destDir = __dirname;
+
+  var videoReadableStream = ytdl(videoUrl, { filter: 'audioonly'});
+
+   ytdl.getInfo(videoUrl, function(err, info){
+     var videoName = info.title.replace('|','').toString('ascii');
+
+     var videoWritableStream = fs.createWriteStream(destDir + '\\' + videoName + '.mp3');
+
+     var stream = videoReadableStream.pipe(videoWritableStream);
+
+     stream.on('finish', function() {
+         res.writeHead(204);
+         res.end();
+     });
+ });
 
 });
 
